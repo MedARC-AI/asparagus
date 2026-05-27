456d14b3917767acd97d83cf7cf866b2ddd1bfc3
Nima Ashjaee <nimaashjaee@gmail.com>
2026-05-27 00:22:26 -0700

    Keep FOMO26 launcher PR limited to SLURM scripts

--- asparagus/modules/datasets/TrainDataset.py
+++ asparagus/modules/datasets/TrainDataset.py
@@ -1,4 +1,3 @@
-import nibabel as nib
 import torch
 import torchvision
 from asparagus.paths import get_data_path, get_source_labels_path
@@ -117,10 +116,7 @@ class SegTestDataset(Dataset):
     def _get_src_label(self, file, properties):
         # source label is the label from the original dataset without any preprocessing
         src_label_path = file.replace(get_data_path(), get_source_labels_path()).replace(".pt", "_label.nii.gz")
-        if src_label_path.endswith((".nii", ".nii.gz")):
-            src_label_nii = nib.load(src_label_path)
-        else:
-            src_label_nii = read_file_to_nifti_or_np(src_label_path)
+        src_label_nii = read_file_to_nifti_or_np(src_label_path)
         src_label_nii = reorient_nib_image(
             src_label_nii,
             original_orientation=properties["original_orientation"],
